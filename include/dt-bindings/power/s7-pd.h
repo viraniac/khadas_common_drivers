82db98ad6955bf637f3f3adbccd9041e699df930
hongyu.chen1 <hongyu.chen1@amlogic.com>
2023-07-17 15:19:15 +0800

    S7: add power domain. [3/3]
    PD#SWPL-131629
    
    Problem:
    s7 need power domain.
    
    Solution:
    add power domain in kernel.
    
    Verify:
    pxp
    
    Change-Id: I4ce47b7de5d734084356a08fc1fdbebe665253ac
    Signed-off-by: hongyu.chen1 <hongyu.chen1@amlogic.com>

---
+++ include/dt-bindings/power/s7-pd.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+
+#define PDID_S7_DOS_HCODEC	0
+#define PDID_S7_DOS_HEVC	1
+#define PDID_S7_DOS_VDEC	2
+#define PDID_S7_VPU_HDMI	3
+#define PDID_S7_USB_COMB		4
+#define PDID_S7_SD_EMMC_C		5
+#define PDID_S7_GE2D		6
+#define PDID_S7_SD_EMMC_A		7
+#define PDID_S7_SD_EMMC_B		8
+#define PDID_S7_ETH		9
+#define PDID_S7_AUCPU		10
+#define PDID_S7_AUDIO		11
