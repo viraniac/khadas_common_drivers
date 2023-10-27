7eb30c87eb25053241569969b27e1b4da02591e9
haitao.liu <haitao.liu@amlogic.com>
2023-10-27 20:17:52 +0800

    vdin: optimize software flow [1/1]
    PD#SWPL-144065
    
    Problem:
    1.vdin wrmif cause od writing over flow
    2.memory leaks in v4l2 mode
    
    Solution:
    1.reset vdin wrmif when starting vdin
    2.Donot get dma buffer one more time in export buffer of vdin
    
    Verify:
    t5m&t7c
    
    Change-Id: Ifd64ca184176047e87b0f6a97a61818e52d32440
    Signed-off-by: haitao.liu <haitao.liu@amlogic.com>

--- drivers/media/vin/tvin/vdin/vdin_ctl.c
+++ drivers/media/vin/tvin/vdin/vdin_ctl.c
@@ -7248,6 +7248,17 @@ void vdin_sw_reset(struct vdin_dev_s *devp)
 	else if (is_meson_t3x_cpu())
 		vdin_sw_reset_t3x(devp);
 #endif
+	if (devp->index) {
+		wr_bits(0, VDIN_TOP_MISC, 1, 27, 1); //vdin1
+		wr_bits(0, VDIN_TOP_MISC, 0, 27, 1); //vdin1
+		wr_bits(0, VDIN_TOP_MISC, 1, 30, 1); //wrmif1
+		wr_bits(0, VDIN_TOP_MISC, 0, 30, 1); //wrmif1
+	} else {
+		wr_bits(0, VDIN_TOP_MISC, 1, 26, 1); //vdin0
+		wr_bits(0, VDIN_TOP_MISC, 0, 26, 1); //vdin0
+		wr_bits(0, VDIN_TOP_MISC, 1, 29, 1); //wrmif0
+		wr_bits(0, VDIN_TOP_MISC, 0, 29, 1); //wrmif0
+	}
 }
 
 void vdin_bist(struct vdin_dev_s *devp, unsigned int mode)
