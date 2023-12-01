f4ef4ef8f5371bb0c3b29d8e623bb2fb3c84d997
hai.cao <hai.cao@amlogic.com>
2023-12-01 16:22:51 +0800

    ge2d: add ge2d support [1/1]
    PD#SWPL-147311
    
    Problem:
    add ge2d support
    
    Solution:
    add support
    
    Verify:
    ptm
    
    Change-Id: I615529360320d2fbbcbfde6eb4e3110ae14c8751
    Signed-off-by: hai.cao <hai.cao@amlogic.com>

--- drivers/media/common/ge2d/ge2d_main.c
+++ drivers/media/common/ge2d/ge2d_main.c
@@ -1685,6 +1685,25 @@ static struct ge2d_device_data_s ge2d_s7 = {
 	.cmd_queue_mode = 0,
 };
 
+static struct ge2d_device_data_s ge2d_s7d = {
+	.ge2d_rate = 667000000,
+	.src2_alp = 1,
+	.canvas_status = 2,
+	.deep_color = 1,
+	.hang_flag = 1,
+	.fifo = 1,
+	.has_self_pwr = 1,
+	.poweron_table = &runtime_poweron_table,
+	.poweroff_table = &runtime_poweroff_table,
+	.chip_type = MESON_CPU_MAJOR_ID_S7D,
+	.adv_matrix = 1,
+	.src2_repeat = 1,
+	.dst_repeat = 1,
+	.dst_sign_mode = 1,
+	.blk_stride_mode = 1,
+	.cmd_queue_mode = 0,
+};
+
 #endif
 #endif
 
@@ -1802,6 +1821,10 @@ static const struct of_device_id ge2d_dt_match[] = {
 	{
 		.compatible = "amlogic, ge2d-s7",
 		.data = &ge2d_s7,
+	},
+	{
+		.compatible = "amlogic, ge2d-s7d",
+		.data = &ge2d_s7d,
 	},
 #endif
 #endif
