f65d0fb0be489352bafab0e167d0c2dfadfb6318
chen.xu <chen.xu@amlogic.com>
2023-12-15 17:00:28 +0800

    amlvideo2: fix panic while screen cap [1/1]
    PD#SWPL-144506
    
    Problem:
    amlvideo2 panic while screen cap.
    
    Solution:
    size is err result data overflow.
    
    Verify:
    T5D
    
    Change-Id: I0818ddce0162b113b5a78430f6862be6dc0d0448
    Signed-off-by: chen.xu <chen.xu@amlogic.com>

--- drivers/media/video_processor/video_dev/amlvideo2.c
+++ drivers/media/video_processor/video_dev/amlvideo2.c
@@ -443,6 +443,7 @@ struct amlvideo2_output {
 	int angle;
 	struct screen_display_info_s info;
 	struct amlvideo2_frame_info *frame;
+	int bsize;
 };
 
 static struct v4l2_frmsize_discrete amlvideo2_prev_resolution[] = {
@@ -3889,6 +3890,7 @@ static int amlvideo2_fillbuff(struct amlvideo2_fh *fh,
 	output.angle = node->qctl_regs[0];
 	output.frame = &buf->axis;
 	output.info.mode = node->mode;
+	output.bsize = buf->vb.bsize;
 	memcpy(&output.info.display_info, &node->display_info,
 	       sizeof(struct vdisplay_info_s));
 
@@ -3919,7 +3921,7 @@ static int amlvideo2_fillbuff(struct amlvideo2_fh *fh,
 	}
 	src_canvas = vf->canvas0Addr;
 	dma_sync_single_for_device(&node->vid_dev->pdev->dev,
-		(dma_addr_t)output.vbuf, node->vid_dev->buffer_size, DMA_TO_DEVICE);
+		(dma_addr_t)output.vbuf, output.bsize, DMA_TO_DEVICE);
 	if (ge2d_proc) {
 #ifdef CONFIG_PM
 		node->could_suspend = false;
