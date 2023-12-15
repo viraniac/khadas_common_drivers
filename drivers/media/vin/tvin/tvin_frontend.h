7b6d0c268e25aa4bb7cd8572fd2324da3dff073c
qiang.liu <qiang.liu@amlogic.com>
2023-12-15 20:54:57 +0800

    tvafe: add wss and snow use tvafe pattern [1/1]
    PD#SWPL-147997
    
    Problem:
    1.wss function not ok
    2.snow display not good
    
    Solution:
    1.add wss function
    2.optimize snow display
    
    Verify:
    t5m
    
    Change-Id: Ie384b65525fc238fc5d44f7ac15c65703a8734b3
    Signed-off-by: qiang.liu <qiang.liu@amlogic.com>

--- drivers/media/vin/tvin/tvin_frontend.h
+++ drivers/media/vin/tvin/tvin_frontend.h
@@ -77,6 +77,7 @@ struct tvin_state_machine_ops_s {
 	void (*hdmi_reset_pcs)(struct tvin_frontend_s *fe);
 	void (*hdmi_de_hactive)(bool en, struct tvin_frontend_s *fe);
 	bool (*hdmi_clr_pkts)(struct tvin_frontend_s *fe);
+	void (*frontend_clr_value)(struct tvin_frontend_s *fe);
 };
 
 struct tvin_frontend_s {
