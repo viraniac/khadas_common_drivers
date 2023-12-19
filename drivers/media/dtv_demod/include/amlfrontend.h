c5e8782cd3f891d2f94276fe3cf90acb49716856
caiyi.xu <caiyi.xu@amlogic.com>
2023-12-19 02:40:03 +0000

    dtv_demod: fix dvbt overflow when 6M 1/4 and other param (v3.5.30) [1/1]
    PD#SWPL-146962
    
    Problem:
    can not work normally on qpsk and qam16
    
    Solution:
    Set different sfec modes based on different modulation
    
    Verify:
    AR331
    
    Change-Id: I10fd6932649e1eeeed2b0fb4624dc6dd8eddbafa
    Signed-off-by: caiyi.xu <caiyi.xu@amlogic.com>

--- drivers/media/dtv_demod/include/amlfrontend.h
+++ drivers/media/dtv_demod/include/amlfrontend.h
@@ -154,6 +154,7 @@
 /*  V3.5.027 Support switching auto between dvb-t and t2 */
 /*  V3.5.028 fix not lock signal in dvbt2 DTG161 case */
 /*  V3.5.029 fix atsc-c signal strength issue */
+/*  V3.5.030 fix dvbt overflow when 6M 1/4 and other param */
 /****************************************************/
 /****************************************************************/
 /*               AMLDTVDEMOD_VER  Description:                  */
@@ -170,8 +171,8 @@
 /*->The last four digits indicate the release time              */
 /****************************************************************/
 #define KERNEL_4_9_EN		1
-#define AMLDTVDEMOD_VER "V3.5.29"
-#define DTVDEMOD_VER	"2023/12/08: fix atsc-c signal strength issue"
+#define AMLDTVDEMOD_VER "V3.5.030"
+#define DTVDEMOD_VER	"2023/12/19: fix dvbt overflow when 6M 1/4 and other param"
 #define AMLDTVDEMOD_T2_FW_VER "20231019_141000"
 #define DEMOD_DEVICE_NAME  "dtvdemod"
 
