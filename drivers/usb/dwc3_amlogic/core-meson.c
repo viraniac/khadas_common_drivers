c8c2f99684f7bb67e7344c9d988d117504e0d0c1
dian.shao <dian.shao@amlogic.com>
2024-01-05 14:58:00 +0800

    usb: tm2 [1/1]
    PD#SWPL-143644
    
    Problem:
    tm2 dwc3 driver default driver
    
    Solution:
    change to dwc3-meson
    
    Verify:
    ab301
    
    Change-Id: If4f36109845563937879f0e3c3fb5aa4bd4fcbb6
    Signed-off-by: dian.shao <dian.shao@amlogic.com>

--- drivers/usb/dwc3_amlogic/core-meson.c
+++ drivers/usb/dwc3_amlogic/core-meson.c
@@ -326,7 +326,6 @@ int aml_dwc3_core_soft_reset(struct aml_dwc3 *dwc)
 	u32		reg;
 	int		ret;
 
-	pr_err("[dwc3 patched:] %s", __func__);
 	/* Before Resetting PHY, put Core in Reset */
 	reg = dwc3_readl(dwc->regs, DWC3_GCTL);
 	reg |= DWC3_GCTL_CORESOFTRESET;
