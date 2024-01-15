683c742a618d155f4a9a3f4721e8ee2fe41ea292
hongyu.chen1 <hongyu.chen1@amlogic.com>
2024-01-15 11:28:02 +0800

    S7D: power domain: modify config. [1/1]
    PD#SWPL-154090
    
    Problem:
    The S7D and S7 power domain names overlap
    and need to be repaired.
    
    Solution:
    modify config.
    
    Verify:
    Compile through.
    
    Change-Id: Ia0d18a5a2684184b8c5938c77b8b7adccd8b0ad6
    Signed-off-by: hongyu.chen1 <hongyu.chen1@amlogic.com>

--- include/dt-bindings/power/s7d-pd.h
+++ include/dt-bindings/power/s7d-pd.h
@@ -3,16 +3,16 @@
  * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
  */
 
-#define PDID_S7_DOS_HCODEC	0
-#define PDID_S7_DOS_HEVC	1
-#define PDID_S7_DOS_VDEC	2
-#define PDID_S7_VPU_HDMI	3
-#define PDID_S7_USB_COMB		4
-#define PDID_S7_SD_EMMC_C		5
-#define PDID_S7_GE2D		6
-#define PDID_S7_SD_EMMC_A		7
-#define PDID_S7_SD_EMMC_B		8
-#define PDID_S7_ETH		9
-#define PDID_S7_AUCPU		10
-#define PDID_S7_AUDIO		11
-#define PDID_S7_AMFC   12
+#define PDID_S7D_DOS_HCODEC	0
+#define PDID_S7D_DOS_HEVC	1
+#define PDID_S7D_DOS_VDEC	2
+#define PDID_S7D_VPU_HDMI	3
+#define PDID_S7D_USB_COMB		4
+#define PDID_S7D_SD_EMMC_C		5
+#define PDID_S7D_GE2D		6
+#define PDID_S7D_SD_EMMC_A		7
+#define PDID_S7D_SD_EMMC_B		8
+#define PDID_S7D_ETH		9
+#define PDID_S7D_AUCPU		10
+#define PDID_S7D_AUDIO		11
+#define PDID_S7D_AMFC   12
