b8e792136972adafd26759e9637150091a7c5b38
Jiacai.Liu <jiacai.liu@amlogic.com>
2024-02-21 19:52:48 +0800

    BT: add config to define the init state of rfkill sw block [1/1]
    PD#SWPL-154847
    
    Problem:
    the init state of rfkill sw block is set to true which will cause the
    power of bt device not applicable.
    
    Solution:
    add CONFIG_AMLOGIC_RFKILL_INIT_SW_UNBLOCK to set the init state of sw
    block as false if the config is defined.
    cherry-pick from: https://scgit.amlogic.com/#/c/391514/
    
    Verify:
    local sc2
    
    Change-Id: I4f23651b1066146066e434a0869c94c625c8d3a9
    Signed-off-by: Jiacai.Liu <jiacai.liu@amlogic.com>

--- drivers/wireless/bluetooth/bt_device.c
+++ drivers/wireless/bluetooth/bt_device.c
@@ -579,8 +579,11 @@ static int bt_probe(struct platform_device *pdev)
 		ret = -ENOMEM;
 		goto err_rfk_alloc;
 	}
-
+#if IS_ENABLED(CONFIG_AMLOGIC_RFKILL_INIT_SW_UNBLOCK)
+	rfkill_init_sw_state(bt_rfk, false);
+#else
 	rfkill_init_sw_state(bt_rfk, true);
+#endif
 	ret = rfkill_register(bt_rfk);
 	if (ret) {
 		pr_err("rfkill_register fail\n");
