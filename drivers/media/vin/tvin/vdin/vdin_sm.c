0e1b3dc902b0522499efc5d94135cf3c6b8ab0fa
haitao.liu <haitao.liu@amlogic.com>
2024-03-05 14:20:30 +0800

    vdin: provider color format for app [1/1]
    PD#SWPL-159339
    
    Problem:
    app cannot get the right input color format
    
    Solution:
    update color format in parm.info,app can get it by
    call ioctl cmd TVIN_IOC_G_FRONTEND_INFO
    
    Verify:
    T5m
    
    Change-Id: I67f1b757873842bfbbd4b4d2b823eece26dad15e
    Signed-off-by: haitao.liu <haitao.liu@amlogic.com>

--- drivers/media/vin/tvin/vdin/vdin_sm.c
+++ drivers/media/vin/tvin/vdin/vdin_sm.c
@@ -218,6 +218,8 @@ static inline void vdin_update_parm(struct vdin_dev_s *devp)
 
 	devp->parm.info.is_dvi = devp->prop.dvi_info;
 	devp->parm.info.fps = devp->prop.fps;
+	devp->parm.info.cfmt =
+		devp->prop.color_format;
 }
 
 /*
