98c5cfaa90277cee6b5bf6941d31a373fa9c2fe6
Evoke Zhang <evoke.zhang@amlogic.com>
2024-03-05 15:16:16 +0800

    lcd: tcon: protect tcon axi memory [2/2]
    PD#SWPL-159745
    
    Problem:
    tcon axi memory wrong access when tcon axi memory failed
    
    Solution:
    add tcon axi memory protect,
    bypass enable od/demura if memory failed
    
    Verify:
    bc302
    
    Change-Id: I34a5d3454cc2d34f9eea0889f3a53e0a8c74c435
    Signed-off-by: Evoke Zhang <evoke.zhang@amlogic.com>

--- drivers/media/vout/lcd/lcd_tcon.c
+++ drivers/media/vout/lcd/lcd_tcon.c
@@ -2966,8 +2966,8 @@ int lcd_tcon_mem_tee_protect(int protect_en)
 {
 	int ret;
 
-	if (!tcon_rmem.axi_rmem) {
-		LCDERR("%s: axi_rmem is null\n", __func__);
+	if (tcon_rmem.flag == 0 || !tcon_rmem.axi_rmem) {
+		LCDERR("%s: no axi_rmem\n", __func__);
 		return -1;
 	}
 
