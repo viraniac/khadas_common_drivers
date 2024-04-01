a1d1313fb61930755251cca44b6c531fe4c4ade2
hongyu.chen1 <hongyu.chen1@amlogic.com>
2024-04-01 10:27:47 +0800

    S7D: add power domain config in kernel. [1/1]
    PD#SWPL-163077
    
    Problem:
    Some new power domains have been added.
    
    Solution:
    Add the relevant configuration.
    
    Verify:
    S7D
    
    Change-Id: I7f5bc148d0034ffad96b1f04f00417ec5796dced
    Signed-off-by: hongyu.chen1 <hongyu.chen1@amlogic.com>

--- include/dt-bindings/power/s7d-pd.h
+++ include/dt-bindings/power/s7d-pd.h
@@ -3,16 +3,17 @@
  * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
  */
 
-#define PDID_S7D_DOS_HCODEC	0
-#define PDID_S7D_DOS_HEVC	1
-#define PDID_S7D_DOS_VDEC	2
-#define PDID_S7D_VPU_HDMI	3
-#define PDID_S7D_USB_COMB		4
-#define PDID_S7D_SD_EMMC_C		5
-#define PDID_S7D_GE2D		6
-#define PDID_S7D_SD_EMMC_A		7
-#define PDID_S7D_SD_EMMC_B		8
-#define PDID_S7D_ETH		9
-#define PDID_S7D_AUCPU		10
-#define PDID_S7D_AUDIO		11
-#define PDID_S7D_AMFC   12
+#define PDID_S7D_DOS_HCODEC		0
+#define PDID_S7D_DOS_HEVC		1
+#define PDID_S7D_DOS_VDEC		2
+#define PDID_S7D_VPU_HDMI		3
+#define PDID_USB_U2DRD			4
+#define PDID_USB_U2H			5
+#define PDID_S7D_SD_EMMC_C		6
+#define PDID_S7D_GE2D			7
+#define PDID_S7D_AMFC			8
+#define PDID_S7D_SD_EMMC_A		9
+#define PDID_S7D_SD_EMMC_B		10
+#define PDID_S7D_ETH			11
+#define PDID_S7D_AUCPU			12
+#define PDID_S7D_AUDIO			13
