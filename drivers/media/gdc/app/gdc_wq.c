9fd68773eb2f4eccdf6b1bf203f301c41236d310
Jian Cao <jian.cao@amlogic.com>
2024-04-19 11:22:16 +0000

    ge2d/gdc: fix the problem of variable memory not being released [1/1]
    PD#SWPL-163798
    
    Problem:
    memory usage keeps increasing
    
    Solution:
    modify logic errors and solve the problem of not being released
    
    Verify:
    s1a
    
    Change-Id: Ia5d1c863a0941067c861d8cd2c8634d26210583c
    Signed-off-by: Jian Cao <jian.cao@amlogic.com>

--- drivers/media/gdc/app/gdc_wq.c
+++ drivers/media/gdc/app/gdc_wq.c
@@ -505,6 +505,7 @@ int gdc_wq_add_work(struct gdc_context_s *wq,
 	int polling_ms = 100;
 	int ret = -1;
 	u32 time_cost;
+	unsigned int block = pitem->cmd.wait_done_flag;
 
 	gdc_log(LOG_DEBUG, "gdc add work\n");
 	spin_lock(&wq->lock);
@@ -515,7 +516,7 @@ int gdc_wq_add_work(struct gdc_context_s *wq,
 	if (gdc_manager.event.cmd_in_sem.count == 0)
 		up(&gdc_manager.event.cmd_in_sem);/* new cmd come in */
 
-	if (pitem->cmd.wait_done_flag) {
+	if (block) {
 		while (1) {
 			ret = wait_event_interruptible_timeout
 					(wq->cmd_complete,
