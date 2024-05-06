c8b3b1bc0a1d07da205a6e4df15ca75e2a8bf784
qianqian.cai <qianqian.cai@amlogic.com>
2024-05-06 20:19:39 +0800

    deinterlace: scatter memory release issue [1/1]
    PD#SWPL-162711
    
    Problem:
    scatter memory release
    
    Solution:
    modify the scatter box release
    
    Verify:
    t3x
    
    Change-Id: I927f9d9e5799915fd55f1e92bfa24a78b2bb2c76
    Signed-off-by: qianqian.cai <qianqian.cai@amlogic.com>

--- drivers/media/di_multi/di_mmu_box.c
+++ drivers/media/di_multi/di_mmu_box.c
@@ -25,7 +25,7 @@ struct di_mmu_box {//decoder_mmu_box
 	const char *name;
 	int channel_id;
 	int tvp_mode;
-	int box_ref_cnt;
+	//int box_ref_cnt;
 	struct mutex mutex;	/* for box */
 	struct list_head list;
 	struct codec_mm_scatter *sc_list[1];
@@ -72,56 +72,6 @@ static int di_mmu_box_mgr_del_box(struct di_mmu_box *box)
 	return 0;
 }
 
-//ary no use now: decoder_xxx, and add 'e' after valid
-bool di_mmu_box_valid_check(void *box)
-{
-	struct di_mmu_box_mgr *mgr = get_decoder_mmu_box_mgr();
-	struct di_mmu_box *mmu_box = NULL;
-	bool is_valid = false;
-
-	mutex_lock(&mgr->mutex);
-	list_for_each_entry(mmu_box, &mgr->box_list, list) {
-		if (mmu_box && mmu_box == box) {
-			is_valid = true;
-			break;
-		}
-	}
-	mutex_unlock(&mgr->mutex);
-
-	return is_valid;
-}
-
-//ary EXPORT_SYMBOL(decoder_mmu_box_valid_check);
-
-//decoder_mmu_try_to_release_box
-//ary no use now
-void di_mmu_try_to_release_box(void *handle)
-{
-	struct di_mmu_box *box = handle;
-	bool is_keep = false;
-	int i;
-
-	if (!box || box->box_ref_cnt)
-		return;
-
-	mutex_lock(&box->mutex);
-	for (i = 0; i < box->max_sc_num; i++) {
-		if (box->sc_list[i]) {
-			is_keep = true;
-			break;
-		}
-	}
-	mutex_unlock(&box->mutex);
-
-	if (!is_keep) {
-		di_mmu_box_mgr_del_box(box);
-			codec_mm_scatter_mgt_delay_free_switch
-				(0, 0, 0, box->tvp_mode);
-		kfree(box);
-	}
-}
-
-//ary EXPORT_SYMBOL(decoder_mmu_try_to_release_box);
 //decoder_mmu_box_sc_check
 int di_mmu_box_sc_check(void *handle, int is_tvp)
 {
@@ -198,7 +148,7 @@ int di_mmu_box_alloc_idx(void *handle, int idx, int num_pages,
 			ret = codec_mm_scatter_alloc_want_pages(sc,
 				num_pages);
 		} else {
-			box->box_ref_cnt--;
+			//box->box_ref_cnt--;
 			codec_mm_scatter_dec_owner_user(sc, 0);
 			box->sc_list[idx] = NULL;
 			sc = NULL;
@@ -214,7 +164,7 @@ int di_mmu_box_alloc_idx(void *handle, int idx, int num_pages,
 			return -1;
 		}
 		box->sc_list[idx] = sc;
-		box->box_ref_cnt++;
+		//box->box_ref_cnt++;
 	}
 
 	for (i = 0; i < num_pages; i++)
@@ -267,13 +217,12 @@ int di_mmu_box_free_idx(void *handle, int idx)
 	if (sc && sc->page_cnt > 0) {
 		codec_mm_scatter_dec_owner_user(sc, 0);
 		box->sc_list[idx] = NULL;
-		box->box_ref_cnt--;
+		//box->box_ref_cnt--;
 	}
+
+	//if (sc && box->box_ref_cnt == 0)
+		//codec_mm_scatter_mgt_delay_free_switch(0, 0, 0, box->tvp_mode);
 	mutex_unlock(&box->mutex);
-
-	if (sc && box->box_ref_cnt == 0)
-		codec_mm_scatter_mgt_delay_free_switch(0, 0, 0, box->tvp_mode);
-
 	return 0;
 }
 
@@ -295,11 +244,17 @@ int di_mmu_box_free(void *handle)
 		if (sc) {
 			codec_mm_scatter_dec_owner_user(sc, 0);
 			box->sc_list[i] = NULL;
+			///box->box_ref_cnt--;
 		}
 	}
+	//if (free_cache && (box->box_ref_cnt == 0)) {
+		//codec_mm_scatter_mgt_delay_free_switch(0, 0, 0, box->tvp_mode);
+	//}
+	codec_mm_scatter_mgt_delay_free_switch(0, 0, 0, box->tvp_mode);
+
 	mutex_unlock(&box->mutex);
 	di_mmu_box_mgr_del_box(box);
-		codec_mm_scatter_mgt_delay_free_switch(0, 0, 0, box->tvp_mode);
+	//codec_mm_scatter_mgt_delay_free_switch(0, 0, 0, box->tvp_mode);
 	kfree(box);
 	return 0;
 }
