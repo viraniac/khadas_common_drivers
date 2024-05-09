0fd9e7bf2658c1609297cc154090d6dd0f112478
Huqiang Qin <huqiang.qin@amlogic.com>
2024-05-09 15:46:08 +0800

    saradc: s7d: optimize driver [1/1]
    PD#SWPL-164728
    
    Problem:
    Optimize driver.
    
    Solution:
    1. Use interrupts to wait for sampling to complete.
    2. Follow upstream advice and remove use of mlock.
    3. Key parameters are configured on DTS.
    4. Add self-test interface.
    5. Add average sampling interface.
    6. Add sampling voltage interface.
    
    Verify:
    S7D/BM201
    
    Change-Id: I81f7f8bc8e45c6ca9ec365c5af8ed09544d5cf4b
    Signed-off-by: Huqiang Qin <huqiang.qin@amlogic.com>

--- drivers/iio/adc/amlogic_saradc.c
+++ drivers/iio/adc/amlogic_saradc.c
@@ -9,11 +9,15 @@
 #include <linux/delay.h>
 #include <linux/io.h>
 #include <linux/iio/iio.h>
+#include <linux/iio/sysfs.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
+#include <linux/of_irq.h>
+#include <linux/interrupt.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
+#include <linux/mutex.h>
 
 #define SARADC_REG0				0x00
 #define SARADC_REG0_SAMPLING_STOP		BIT(14)
@@ -74,6 +78,15 @@
 
 #define SARADC_CHANNEL_MAX			8
 #define SARADC_MAX_FIFO_SIZE			16
+#define SARADC_TIMEOUT				100	/* Millisecond */
+
+#define SARADC_DEFAULT_CLOCK_FREQUENCY		1200000
+#define SARADC_DEFAULT_TEST_CHANNEL		7
+#define SARADC_DEFAULT_FIFO_DATA_WIDTH		16
+#define SARADC_DEFAULT_VREF_VOLTAGE		1800000	/* Microvolt */
+
+/* For IIO_VAL_FRACTIONAL use */
+#define THOUSAND				1000
 
 enum amlogic_saradc_avg_mode {
 	NO_AVERAGING = 0x0,
@@ -95,15 +108,32 @@ struct amlogic_saradc_priv {
 	struct clk *clk_core;
 	struct clk *clk_gate;
 	struct clk *clk_mux;
+	u32 clock_frequency;
+	u32 test_channel;
+	u32 fifo_data_width;
 	int info_offset;
 	int info_scale;
+	struct completion sample_done;
+	/*
+	 * This lock ensures that there is only one place to operate
+	 * the controller at the same time.
+	 */
+	struct mutex lock;
+};
+
+static const char * const test_voltage[] = {
+	"gnd",
+	"vdd/4",
+	"vdd/2",
+	"vdd*3/4",
+	"vdd",
 };
 
 static void amlogic_saradc_set_averaging(struct amlogic_saradc_priv *priv, int address,
 					 enum amlogic_saradc_avg_mode mode,
 					 enum amlogic_saradc_num_samples samples)
 {
-	int val;
+	unsigned int val;
 
 	val = samples << SARADC_REG2_NUM_SAMPLES_SHIFT(address);
 	regmap_update_bits(priv->regmap, SARADC_REG2,
@@ -126,7 +156,8 @@ static void amlogic_saradc_enable_channel(struct amlogic_saradc_priv *priv,
 
 static int amlogic_saradc_wait_busy_clear(struct amlogic_saradc_priv *priv)
 {
-	int regval, timeout = 20000;
+	u32 regval;
+	int timeout = SARADC_TIMEOUT * 1000;
 
 	do {
 		usleep_range(1, 2);
@@ -157,6 +188,10 @@ static int amlogic_saradc_read_fifo(struct amlogic_saradc_priv *priv, int *value
 {
 	unsigned int count, tmp;
 
+	if (!wait_for_completion_timeout(&priv->sample_done,
+					 msecs_to_jiffies(SARADC_TIMEOUT)))
+		return -ETIMEDOUT;
+
 	regmap_read(priv->regmap, SARADC_STATUS0, &tmp);
 	count = FIELD_GET(SARADC_STATUS0_FIFO_COUNT_MASK, tmp);
 	if (count != 1)
@@ -164,19 +199,34 @@ static int amlogic_saradc_read_fifo(struct amlogic_saradc_priv *priv, int *value
 
 	regmap_read(priv->regmap, SARADC_STATUS3, &tmp);
 
-	*value = tmp & 0xffff;
+	*value = tmp & (BIT(priv->fifo_data_width) - 1);
 
 	return 0;
 }
 
+static void amlogic_saradc_start_sample(struct amlogic_saradc_priv *priv)
+{
+	reinit_completion(&priv->sample_done);
+
+	regmap_update_bits(priv->regmap, SARADC_REG0,
+			   SARADC_REG0_SAMPLE_START,
+			   SARADC_REG0_SAMPLE_START);
+}
+
 static int amlogic_saradc_get_sample(struct iio_dev *indio_dev,
-				     const struct iio_chan_spec *chan, int *val)
+				     const struct iio_chan_spec *chan,
+				     int *val, bool en_avg)
 {
 	struct amlogic_saradc_priv *priv = iio_priv(indio_dev);
 	int ret;
 	int raw;
 
-	mutex_lock(&indio_dev->mlock);
+	mutex_lock(&priv->lock);
+
+	/* Configure the averaging mode of the channels we use */
+	amlogic_saradc_set_averaging(priv, 0,
+				     en_avg ? MEDIAN_AVERAGING : NO_AVERAGING,
+				     en_avg ? EIGHT_SAMPLES : ONE_SAMPLE);
 
 	/* Set channel */
 	amlogic_saradc_enable_channel(priv, chan->address, 0);
@@ -184,13 +234,7 @@ static int amlogic_saradc_get_sample(struct iio_dev *indio_dev,
 	amlogic_saradc_clear_fifo(priv);
 
 	/* Start sample */
-	regmap_update_bits(priv->regmap, SARADC_REG0,
-			   SARADC_REG0_SAMPLE_START,
-			   SARADC_REG0_SAMPLE_START);
-
-	ret = amlogic_saradc_wait_busy_clear(priv);
-	if (ret)
-		goto fail;
+	amlogic_saradc_start_sample(priv);
 
 	ret = amlogic_saradc_read_fifo(priv, &raw);
 	if (ret)
@@ -198,13 +242,39 @@ static int amlogic_saradc_get_sample(struct iio_dev *indio_dev,
 
 	*val = (short)raw;
 
-	mutex_unlock(&indio_dev->mlock);
+	mutex_unlock(&priv->lock);
 
 	return IIO_VAL_INT;
 
 fail:
-	mutex_unlock(&indio_dev->mlock);
+	mutex_unlock(&priv->lock);
 	return ret;
+}
+
+static int amlogic_saradc_get_sample_voltage(struct iio_dev *indio_dev,
+					     const struct iio_chan_spec *chan,
+					     int *val, bool en_avg)
+{
+	struct amlogic_saradc_priv *priv = iio_priv(indio_dev);
+	int sample_value;
+	int voltage;
+	int ret;
+
+	ret = amlogic_saradc_get_sample(indio_dev, chan, &sample_value, en_avg);
+	if (ret < 0)
+		return ret;
+
+	/* Convert to voltage value (unit: millivolt) */
+	voltage = sample_value + priv->info_offset;
+	voltage = voltage > 0 ? voltage : 0;
+	voltage = voltage * priv->info_scale;
+	voltage = voltage < SARADC_DEFAULT_VREF_VOLTAGE ? voltage :
+		  SARADC_DEFAULT_VREF_VOLTAGE;
+	voltage /= THOUSAND;
+
+	*val = voltage;
+
+	return IIO_VAL_INT;
 }
 
 static int amlogic_saradc_iio_info_read_raw(struct iio_dev *indio_dev,
@@ -215,7 +285,14 @@ static int amlogic_saradc_iio_info_read_raw(struct iio_dev *indio_dev,
 
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
-		return amlogic_saradc_get_sample(indio_dev, chan, val);
+		return amlogic_saradc_get_sample(indio_dev, chan, val, false);
+
+	case IIO_CHAN_INFO_AVERAGE_RAW:
+		return amlogic_saradc_get_sample(indio_dev, chan, val, true);
+
+	case IIO_CHAN_INFO_PROCESSED:
+		return amlogic_saradc_get_sample_voltage(indio_dev, chan,
+							 val, true);
 
 	case IIO_CHAN_INFO_OFFSET:
 		*val = priv->info_offset;
@@ -223,45 +300,62 @@ static int amlogic_saradc_iio_info_read_raw(struct iio_dev *indio_dev,
 
 	case IIO_CHAN_INFO_SCALE:
 		*val = priv->info_scale;
-		*val2 = 1000;
+		*val2 = THOUSAND;
 		return IIO_VAL_FRACTIONAL;
 	}
 
 	return -EINVAL;
 }
 
-static int amlogic_saradc_self_calib(struct iio_dev *indio_dev)
+static void amlogic_saradc_set_test_input(struct iio_dev *indio_dev, int sel)
 {
+	u32 regval;
 	struct amlogic_saradc_priv *priv = iio_priv(indio_dev);
-	unsigned int regval;
-	int raw_gnd;
-	int raw_vref;
-	int ret;
 
-	/* Set to GND */
-	regval = FIELD_PREP(SARADC_REG11_CHANNEL7_AUX_CTRL_MASK, 0);
+	regval = FIELD_PREP(SARADC_REG11_CHANNEL7_AUX_CTRL_MASK, sel);
 	regmap_update_bits(priv->regmap, SARADC_REG11,
 			   SARADC_REG11_CHANNEL7_AUX_CTRL_MASK, regval);
 	usleep_range(10, 20);
-	ret = amlogic_saradc_get_sample(indio_dev, &priv->iio_channels[7], &raw_gnd);
+}
+
+static int amlogic_saradc_get_test_input(struct iio_dev *indio_dev)
+{
+	u32 regval;
+	int sel;
+	struct amlogic_saradc_priv *priv = iio_priv(indio_dev);
+
+	regmap_read(priv->regmap, SARADC_REG11, &regval);
+	sel = FIELD_GET(SARADC_REG11_CHANNEL7_AUX_CTRL_MASK, regval);
+
+	return sel;
+}
+
+static int amlogic_saradc_self_calib(struct iio_dev *indio_dev)
+{
+	struct amlogic_saradc_priv *priv = iio_priv(indio_dev);
+	int raw_gnd;
+	int raw_vref;
+	int ret;
+	struct iio_chan_spec *chan = &priv->iio_channels[priv->test_channel];
+
+	/* Set to GND */
+	amlogic_saradc_set_test_input(indio_dev, 0);
+	ret = amlogic_saradc_get_sample(indio_dev, chan, &raw_gnd, true);
 	if (ret < 0) {
 		dev_err(indio_dev->dev.parent, "self-calibration failed: sample error\n");
 		return ret;
 	}
 
 	/* Set to VREF */
-	regval = FIELD_PREP(SARADC_REG11_CHANNEL7_AUX_CTRL_MASK, 4);
-	regmap_update_bits(priv->regmap, SARADC_REG11,
-			   SARADC_REG11_CHANNEL7_AUX_CTRL_MASK, regval);
-	usleep_range(10, 20);
-	ret = amlogic_saradc_get_sample(indio_dev, &priv->iio_channels[7], &raw_vref);
+	amlogic_saradc_set_test_input(indio_dev, 4);
+	ret = amlogic_saradc_get_sample(indio_dev, chan, &raw_vref, true);
 	if (ret < 0) {
 		dev_err(indio_dev->dev.parent, "self-calibration failed: sample error\n");
 		return ret;
 	}
 
 	priv->info_offset = -raw_gnd;
-	priv->info_scale = 1800 * 1000 / (raw_vref - raw_gnd);
+	priv->info_scale = SARADC_DEFAULT_VREF_VOLTAGE / (raw_vref - raw_gnd);
 
 	if (priv->info_scale <= 0) {
 		dev_err(indio_dev->dev.parent, "self-calibration failed: gnd=%d, vref=%d\n",
@@ -284,7 +378,7 @@ static int amlogic_saradc_init(struct iio_dev *indio_dev)
 		return ret;
 	}
 
-	ret = clk_set_rate(priv->clk_gate, 1200000);
+	ret = clk_set_rate(priv->clk_gate, priv->clock_frequency);
 	if (ret) {
 		dev_err(indio_dev->dev.parent, "failed to set adc clock rate\n");
 		return ret;
@@ -292,29 +386,25 @@ static int amlogic_saradc_init(struct iio_dev *indio_dev)
 
 	usleep_range(5, 10);
 
-	/* filter Control */
+	/* Filter control */
 	regmap_write(priv->regmap, SARADC_REG7, 0x00000c21);
 	regmap_write(priv->regmap, SARADC_REG8, 0x0280614d);
 
-	/* delay */
+	/* Delay configure */
 	regmap_write(priv->regmap, SARADC_REG3, 0x10a02403);
 	regmap_write(priv->regmap, SARADC_REG4, 0x00000080);
-	regmap_write(priv->regmap, SARADC_REG5, 0x0010341b);
+	regmap_write(priv->regmap, SARADC_REG5, 0x0010340b);
+
+	/* Control */
 	regmap_write(priv->regmap, SARADC_REG6, 0x00000031);
 
-	/* set aux and extern vref */
+	/* Set aux and extern vref */
 	regmap_write(priv->regmap, SARADC_REG9, 0x0000e4e4);
 	regmap_write(priv->regmap, SARADC_REG10, 0x74543414);
 	regmap_write(priv->regmap, SARADC_REG11, 0xf4d4b494);
 
-	/* other */
-	regmap_write(priv->regmap, SARADC_REG12, 0x00000000);
-	regmap_write(priv->regmap, SARADC_REG13, 0x00000000);
-	regmap_write(priv->regmap, SARADC_REG14, 0x00000000);
-
+	/* ADC is disabled by default */
 	regmap_write(priv->regmap, SARADC_REG0, 0x00400000);
-
-	amlogic_saradc_set_averaging(priv, 0, MEDIAN_AVERAGING, EIGHT_SAMPLES);
 
 	return 0;
 }
@@ -323,8 +413,9 @@ static int amlogic_saradc_hw_enable(struct iio_dev *indio_dev)
 {
 	struct amlogic_saradc_priv *priv = iio_priv(indio_dev);
 	int ret;
+	u32 regval;
 
-	mutex_lock(&indio_dev->mlock);
+	mutex_lock(&priv->lock);
 
 	ret = clk_prepare_enable(priv->clk_core);
 	if (ret) {
@@ -351,16 +442,26 @@ static int amlogic_saradc_hw_enable(struct iio_dev *indio_dev)
 	regmap_update_bits(priv->regmap, SARADC_REG0,
 			   SARADC_REG0_SAMPLING_STOP, 0);
 
+	/* An interrupt will be generated when there is data in the FIFO */
+	regval = FIELD_PREP(SARADC_REG0_FIFO_CNT_IRQ_MASK, 1);
+	regmap_update_bits(priv->regmap, SARADC_REG0,
+			   SARADC_REG0_FIFO_CNT_IRQ_MASK, regval);
+
+	/* Enable FIFO interrupt */
+	regmap_update_bits(priv->regmap, SARADC_REG0,
+			   SARADC_REG0_FIFO_IRQ_EN,
+			   SARADC_REG0_FIFO_IRQ_EN);
+
 	amlogic_saradc_clear_fifo(priv);
 
-	mutex_unlock(&indio_dev->mlock);
+	mutex_unlock(&priv->lock);
 
 	return 0;
 
 err_gate:
 	clk_disable_unprepare(priv->clk_core);
 err_core:
-	mutex_unlock(&indio_dev->mlock);
+	mutex_unlock(&priv->lock);
 	return ret;
 }
 
@@ -368,7 +469,11 @@ static int amlogic_saradc_hw_disable(struct iio_dev *indio_dev)
 {
 	struct amlogic_saradc_priv *priv = iio_priv(indio_dev);
 
-	mutex_lock(&indio_dev->mlock);
+	mutex_lock(&priv->lock);
+
+	/* Disable FIFO interrupt */
+	regmap_update_bits(priv->regmap, SARADC_REG0,
+			   SARADC_REG0_FIFO_IRQ_EN, 0);
 
 	regmap_update_bits(priv->regmap, SARADC_REG0,
 			   SARADC_REG0_SAMPLING_STOP,
@@ -384,13 +489,62 @@ static int amlogic_saradc_hw_disable(struct iio_dev *indio_dev)
 	clk_disable_unprepare(priv->clk_gate);
 	clk_disable_unprepare(priv->clk_core);
 
-	mutex_unlock(&indio_dev->mlock);
+	mutex_unlock(&priv->lock);
 
 	return 0;
 }
 
+static ssize_t test_input_show(struct device *dev, struct device_attribute *attr,
+			       char *buf)
+{
+	struct iio_dev *indio_dev = dev_to_iio_dev(dev);
+	struct amlogic_saradc_priv *priv = iio_priv(indio_dev);
+	int sel = amlogic_saradc_get_test_input(indio_dev);
+	int len = 0;
+	int index;
+
+	len += sprintf(buf + len, "test channel: %d\n", priv->test_channel);
+	len += sprintf(buf + len, "current: %d ", sel);
+	if (sel < ARRAY_SIZE(test_voltage))
+		len += sprintf(buf + len, "(%s)",  test_voltage[sel]);
+	len += sprintf(buf + len, "\n\n");
+
+	for (index = 0; index < ARRAY_SIZE(test_voltage); index++)
+		len += sprintf(buf + len, "%d: %s\n", index, test_voltage[index]);
+
+	return len;
+}
+
+static ssize_t test_input_store(struct device *dev, struct device_attribute *attr,
+				const char *buf, size_t count)
+{
+	struct iio_dev *indio_dev = dev_to_iio_dev(dev);
+	int sel;
+
+	if (kstrtoint(buf, 0, &sel) != 0)
+		return -EINVAL;
+	if (sel >= ARRAY_SIZE(test_voltage))
+		return -EINVAL;
+
+	amlogic_saradc_set_test_input(indio_dev, sel);
+
+	return count;
+}
+
+static IIO_DEVICE_ATTR(test_input, 0644, test_input_show, test_input_store, -1);
+
+static struct attribute *amlogic_saradc_attrs[] = {
+	&iio_dev_attr_test_input.dev_attr.attr,
+	NULL,
+};
+
+static const struct attribute_group amlogic_saradc_attr_group = {
+	.attrs = amlogic_saradc_attrs,
+};
+
 static const struct iio_info amlogic_saradc_iio_info = {
 	.read_raw = amlogic_saradc_iio_info_read_raw,
+	.attrs = &amlogic_saradc_attr_group,
 };
 
 static const struct regmap_config amlogic_saradc_regmap_config = {
@@ -405,21 +559,35 @@ static const struct of_device_id amlogic_saradc_of_match[] = {
 };
 MODULE_DEVICE_TABLE(of, amlogic_saradc_of_match);
 
+static irqreturn_t amlogic_saradc_fifo_irq(int irq, void *data)
+{
+	struct iio_dev *indio_dev = data;
+	struct amlogic_saradc_priv *priv = iio_priv(indio_dev);
+
+	complete(&priv->sample_done);
+
+	return IRQ_HANDLED;
+}
+
 static int amlogic_saradc_probe(struct platform_device *pdev)
 {
 	struct amlogic_saradc_priv *priv;
+	struct device *dev = &pdev->dev;
+	struct device_node *np = dev->of_node;
 	struct iio_dev *indio_dev;
 	void __iomem *base;
-	int index, ret;
+	int index;
+	int ret;
+	int fifo_irq;
 
-	indio_dev = devm_iio_device_alloc(&pdev->dev, sizeof(*priv));
-	if (!indio_dev) {
-		dev_err(&pdev->dev, "failed allocating iio device\n");
-		return -ENOMEM;
-	}
+	indio_dev = devm_iio_device_alloc(dev, sizeof(*priv));
+	if (!indio_dev)
+		return dev_err_probe(dev, -ENOMEM, "failed allocating iio device\n");
+
 	priv = iio_priv(indio_dev);
+	init_completion(&priv->sample_done);
 
-	indio_dev->name = dev_name(&pdev->dev);
+	indio_dev->name = dev_name(dev);
 	indio_dev->modes = INDIO_DIRECT_MODE;
 	indio_dev->info = &amlogic_saradc_iio_info;
 
@@ -430,7 +598,9 @@ static int amlogic_saradc_probe(struct platform_device *pdev)
 		priv->iio_channels[index].channel = index;
 		priv->iio_channels[index].address = index;
 		priv->iio_channels[index].info_mask_separate =
-					BIT(IIO_CHAN_INFO_RAW);
+					BIT(IIO_CHAN_INFO_RAW) |
+					BIT(IIO_CHAN_INFO_AVERAGE_RAW) |
+					BIT(IIO_CHAN_INFO_PROCESSED);
 		priv->iio_channels[index].info_mask_shared_by_type =
 					BIT(IIO_CHAN_INFO_SCALE) |
 					BIT(IIO_CHAN_INFO_OFFSET);
@@ -438,50 +608,73 @@ static int amlogic_saradc_probe(struct platform_device *pdev)
 	indio_dev->channels = priv->iio_channels;
 	indio_dev->num_channels = SARADC_CHANNEL_MAX;
 
+	ret = of_property_read_u32(np, "amlogic,clock-frequency",
+				   &priv->clock_frequency);
+	if (ret)
+		priv->clock_frequency = SARADC_DEFAULT_CLOCK_FREQUENCY;
+	dev_dbg(dev, "clock-frequency: %u\n", priv->clock_frequency);
+
+	ret = of_property_read_u32(np, "amlogic,test-channel",
+				   &priv->test_channel);
+	if (ret || priv->test_channel >= SARADC_CHANNEL_MAX)
+		priv->test_channel = SARADC_DEFAULT_TEST_CHANNEL;
+	dev_dbg(dev, "test-channel: %u\n", priv->test_channel);
+
+	ret = of_property_read_u32(np, "amlogic,fifo-data-width",
+				   &priv->fifo_data_width);
+	if (ret)
+		priv->fifo_data_width = SARADC_DEFAULT_FIFO_DATA_WIDTH;
+	dev_dbg(dev, "fifo-data-width: %u\n", priv->fifo_data_width);
+
 	base = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(base))
 		return PTR_ERR(base);
 
-	priv->regmap = devm_regmap_init_mmio(&pdev->dev, base,
+	priv->regmap = devm_regmap_init_mmio(dev, base,
 					     &amlogic_saradc_regmap_config);
 	if (IS_ERR(priv->regmap))
 		return PTR_ERR(priv->regmap);
 
-	priv->clk_src = devm_clk_get(&pdev->dev, "src");
-	if (IS_ERR(priv->clk_src)) {
-		dev_err(&pdev->dev, "failed to get src clk\n");
-		return PTR_ERR(priv->clk_src);
-	}
+	fifo_irq = irq_of_parse_and_map(dev->of_node, 0);
+	if (!fifo_irq)
+		return dev_err_probe(dev, -EINVAL, "failed to get fifo irq\n");
 
-	priv->clk_core = devm_clk_get(&pdev->dev, "core");
-	if (IS_ERR(priv->clk_core)) {
-		dev_err(&pdev->dev, "failed to get core clk\n");
-		return PTR_ERR(priv->clk_core);
-	}
+	ret = devm_request_irq(dev, fifo_irq, amlogic_saradc_fifo_irq,
+			       IRQF_SHARED, dev_name(dev), indio_dev);
+	if (ret)
+		return dev_err_probe(dev, ret, "failed to request fifo irq\n");
 
-	priv->clk_gate = devm_clk_get(&pdev->dev, "gate");
+	priv->clk_src = devm_clk_get(dev, "src");
+	if (IS_ERR(priv->clk_src))
+		return dev_err_probe(dev, PTR_ERR(priv->clk_src), "failed to get src clk\n");
+
+	priv->clk_core = devm_clk_get(dev, "core");
+	if (IS_ERR(priv->clk_core))
+		return dev_err_probe(dev, PTR_ERR(priv->clk_core), "failed to get core clk\n");
+
+	priv->clk_gate = devm_clk_get(dev, "gate");
 	if (IS_ERR(priv->clk_gate)) {
-		if (PTR_ERR(priv->clk_gate) == -ENOENT) {
+		ret = PTR_ERR(priv->clk_gate);
+		if (ret == -ENOENT)
 			priv->clk_gate = NULL;
-		} else {
-			dev_err(&pdev->dev, "failed to get gate clk\n");
-			return PTR_ERR(priv->clk_gate);
-		}
+		else
+			return dev_err_probe(dev, ret, "failed to get gate clk\n");
 	}
 
-	priv->clk_mux = devm_clk_get(&pdev->dev, "mux");
+	priv->clk_mux = devm_clk_get(dev, "mux");
 	if (IS_ERR(priv->clk_mux)) {
-		if (PTR_ERR(priv->clk_mux) == -ENOENT) {
+		ret = PTR_ERR(priv->clk_mux);
+		if (ret == -ENOENT)
 			priv->clk_mux = NULL;
-		} else {
-			dev_err(&pdev->dev, "failed to get mux clk\n");
-			return PTR_ERR(priv->clk_mux);
-		}
+		else
+			return dev_err_probe(dev, ret, "failed to get mux clk\n");
 	}
 
 	ret = amlogic_saradc_init(indio_dev);
 	if (ret)
 		goto err;
+
+	mutex_init(&priv->lock);
 
 	ret = amlogic_saradc_hw_enable(indio_dev);
 	if (ret)
